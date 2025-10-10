c9415ad36a3234e03452c49b95ee7acdd0dd5fbc
Graham Kelly <gkgoat6700@gmail.com>
2025-10-09 20:15:25 -0400

    init

--- _boot.sh
+++ _boot.sh
@@ -1,2 +1,2 @@
 cd $(dirname $0)
-unshare -pfr --user --mount --kill-child $(which gocryptfs) $(pwd) $1 
+unshare -pfr --user --mount --kill-child $(which bash) -c '$@; exec $(which bash) -i;' -- $(which gocryptfs) $(pwd) $1 
